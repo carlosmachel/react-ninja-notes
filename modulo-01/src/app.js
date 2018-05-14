afef32734c3b57f3536bb47ca152116b8c078547
Carlos Machel <carlos.git@outlook.com>
2018-05-14 10:51:44 -0300

    learning about events

--- modulo-01/src/app.js
+++ modulo-01/src/app.js
@@ -1,13 +1,15 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
+import Square from './square'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <Title name='Carlos' lastName='Machel' />
+      <div className='container' onClick={e => alert('clicou')}>
+        {['blue', 'red', 'green'].map(color => (
+          <Square color={color} key={color} />
+        ))}
       </div>
     )
   }
